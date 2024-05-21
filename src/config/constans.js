0f7a770fad94d9a0ea78353ab78ea4284136a700
aguerreroperez1973 <aguerreroperez@hotmail.com>
2024-05-21 12:14:59 -0400

    Se modifica url

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,6 +1,6 @@
 //export const URLBASE = 'http://127.0.0.1:3000'
-//export const URLBASE = 'http://incotel2.ddns.net:3000'
-export const URLBASE = 'http://192.168.1.82:3000'
+export const URLBASE = 'http://incotel2.ddns.net:3000'
+//export const URLBASE = 'http://192.168.1.82:3000'
 
 export const ENDPOINT = {
   login: `${URLBASE}/login/`,
